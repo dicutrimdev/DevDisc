6eadc6218abd44a99e49813dce082364e4426a74
dicutrimdev <diegocutrimcontato@hotmail.com>
2025-06-03 16:08:45 -0300

    Criação do projeto e atualização do arquivo .gitignore

---
+++ src/main/java/com/dominio/devdisc/DevdiscApplication.java
@@ -0,0 +1,13 @@
+package com.dominio.devdisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdiscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominio/devdisc/DevdiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominio.devdisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
